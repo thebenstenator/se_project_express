1a7297b4b0c9d5377cb2be6099edab565d692c32
thebenstenator <thebenstenator@gmail.com>
2026-02-19 09:51:53 -0700

    add logging through winston

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const cors = require("cors");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(requestLogger);
 app.use("/", mainRouter);
-
+app.use(errorLogger);
 app.use(errors());
-
 app.use(errorHandler);
 
 mongoose
